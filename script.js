2d3c6eccff10ca9247620050d90a4901180d2ea1
Michael <55346444+hellawi@users.noreply.github.com>
2023-03-02 20:28:57 -0800

    added 2 march

---
+++ script.js
@@ -0,0 +1,13 @@
+let input = document.querySelector("#year_birth")
+let button = document.querySelector(".fa-search")
+let result = document.querySelector(".birth_result")
+button.addEventListener("click", function(){
+    if(input.value % 4 == 0){
+        result.innerHTML = "Ви народились в високосний рік!"
+        result.style.color = "green"
+    } else{
+        result.innerHTML = "Ви народились не в високосний рік!"
+        result.style.color = "red"
+        result.style.fontWeight = "bold"
+    }
+})
