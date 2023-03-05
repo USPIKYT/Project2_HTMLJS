136a9a29abedaae92d54b736d7a79a8807686cf8
Michael <55346444+hellawi@users.noreply.github.com>
2023-03-04 21:32:33 -0800

    added 3 commit hft1

--- script.js
+++ script.js
@@ -11,3 +11,23 @@ button.addEventListener("click", function(){
         result.style.fontWeight = "bold"
     }
 })
+
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// C
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
