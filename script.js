db0afa4e37ceab7a6137b4ca3cb9ca4d277a02cc
Michael <55346444+hellawi@users.noreply.github.com>
2023-03-05 10:23:48 -0800

    Added header and footer

--- script.js
+++ script.js
@@ -12,22 +12,26 @@ button.addEventListener("click", function(){
     }
 })
 
-
 function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
+  document.getElementById("myDropdown").classList.toggle("show");
 }
 
-// C
+// Close the dropdown menu if the user clicks outside of it
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
 // Close the dropdown if the user clicks outside of it
 window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
+if (!event.target.matches('.dropbtn')) {
 
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
+  var dropdowns = document.getElementsByClassName("dropdown-content");
+  var i;
+  for (i = 0; i < dropdowns.length; i++) {
+    var openDropdown = dropdowns[i];
+    if (openDropdown.classList.contains('show')) {
+      openDropdown.classList.remove('show');
     }
   }
+}
+}
